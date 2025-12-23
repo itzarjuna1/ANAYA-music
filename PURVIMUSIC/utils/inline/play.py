2de2aa324472f39ab1266c4483d68b6119672c9c
ARJUN <pachangedinanath@gmail.com>
2025-12-23 17:16:10 +0530

    Update play.py

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -33,23 +33,23 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "─🅢─────────"
+        bar = "─🐾─────────"
     elif 10 < umm < 20:
-        bar = "──🅞────────"
+        bar = "──🐾────────"
     elif 20 <= umm < 30:
-        bar = "───🅝───────"
+        bar = "───🐾───────"
     elif 30 <= umm < 40:
-        bar = "────🅐──────"
+        bar = "────🐾──────"
     elif 40 <= umm < 50:
         bar = "─────♡─────"
     elif 50 <= umm < 60:
-        bar = "──────🅡────"
+        bar = "──────🐾────"
     elif 60 <= umm < 70:
-        bar = "───────🅐───"
+        bar = "───────🐾───"
     elif 70 <= umm < 80:
-        bar = "────────🅙──"
+        bar = "────────🐾──"
     elif 80 <= umm < 95:
-        bar = "─────────🅐─"   
+        bar = "─────────🐾─"   
     else:
         bar = "──────────♡"
         
@@ -69,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/PURVI_SUPPORT",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/owner_of_itachi",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/PURVI_UPDATES",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/snowy_hometown",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -91,10 +91,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/PURVI_SUPPORT",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/owner_of_itachi",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/PURVI_UPDATES",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/snowy_hometown",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
